1d8a69a72144422d9a121cfbc4bb7dec314b4359
liuli <1751336358@qq.com>
2023-01-21 17:38:14 +0800

    callback init

--- src/main/java/com/game/manager/AccountManager.java
+++ src/main/java/com/game/manager/AccountManager.java
@@ -6,14 +6,19 @@ import com.game.core.dto.User;
 import com.game.service.proxy.AccountServiceProxy;
 
 public class AccountManager {
-    public void verify(HumanObject humanObj, User user){
+    public void verify(HumanObject humanObj, User user) {
         Integer humanId = humanObj.getHumanId();
         Connection conn = humanObj.getConnection();
         int portId = conn.getPort().getPortId();
         AccountServiceProxy prx = new AccountServiceProxy(conn.getPort());
-        prx.verify(user.getName(),user.getAge());
-        String result = "verify return portId = " + portId +", humanId = " + humanId + ",param = " + user;
+        prx.verify(user.getName(), user.getAge());
+
+        String result = "verify return portId = " + portId + ", humanId = " + humanId + ",param = " + user;
         humanObj.sendMsg(result);
+    }
+
+    public void _result(Object result,Object ... context) {
+
     }
 
 
